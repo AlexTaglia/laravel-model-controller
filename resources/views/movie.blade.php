7c7bf82c9c9e4ee2ce4da4336de1701680873a30
AlexTaglia <alex.tagliabue@gmail.com>
2021-09-09 16:47:03 +0200

    Aggiunta style

--- resources/views/movie.blade.php
+++ resources/views/movie.blade.php
@@ -19,22 +19,38 @@ BONUS: Stilare il layout nei dettagli con Sass
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="{{ mix('/css/app.css') }}">
     <title>laravel model controller</title>
 </head>
 <body>
-    <h1>Avengers films</h1>
+    <div class="container">
+        <h1>Avengers films</h1>
+    
+        @foreach ($allMovies as $movie)
+            <div class="film-section">
+                
+                <h2>{{$movie->title}}</h2>
+                <div class="top flex">
+                    <div class="img">
+                        <img src="{{$movie->poster}}" alt="">
+                    </div>
+                    <div class="details">
+                        <p><b>Diretto da: </b>{{$movie->directed_by}}</p>
+                        <p><b>Durata: </b>{{$movie->running_time}} minuti</p>
+                        <p><b>Uscita: </b>{{$movie->release_date}}</p>
+                        <p><b>Casa di produzione: </b>{{$movie->production_company}}</p>
+                        <p><b>Attori principali: </b>{{$movie->starring}}</p>
+                    </div>
+                </div>
 
-    @foreach ($allMovies as $movie)
-        <div class="film-section">
-            <h4>{{$movie->title}}</h4>
-            <p>{{$movie->directed_by}}</p>
-            <p>{{$movie->running_time}}</p>
-            <p>{{$movie->release_date}}</p>
-            <p>{{$movie->production_company}}</p>
-            <p>{{$movie->starring}}</p>
-            <p>{{$movie->plot}}</p>
-        </div>    
-    @endforeach
+                <div class="bottom">
+                    <h4>Trama</h4>
+                    <p>{{$movie->plot}}</p>
+                </div>
+            </div>    
+        @endforeach
+
+    </div>
     
 </body>
 </html>
